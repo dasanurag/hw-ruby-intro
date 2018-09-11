ea7fcce5595b743e73de8c4b9e07dedd14b876d3
anurag <das.anurag2012@gmail.com>
2018-09-11 15:37:40 -0500

    Homework Completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,61 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+  for i in 0 .. arr.length - 1
+	total += arr[i]
+  end
+  return total	
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0 
+	return 0
+  elseif arr.length == 1
+	return arr.max
+  else
+	return sum(arr.max(2))
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  !!arr.combination(2).detect { |a,b| a+b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}" 
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return !!(s =~ /^[b-df-hj-np-tv-z]/ or s =~ /^[B-DF-HJ-NP-TV-Z]/)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[01]*$/ and s.length > 0
+	return s.to_i(base=2).modulo(4).zero?
+  else
+	return false
+  end	
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn,price)
+	if isbn.length == 0 or price <= 0
+		raise ArgumentError.new("Either isbn is empty or price is less than or equal to zero!!")
+	end
+	@isbn = isbn
+	@price = price
+  end
+
+  def price_as_string
+#	@price = @price.to_f.round(3)
+	@price = sprintf("%0.02f",@price)
+	return "$#{@price}"
+  end
 end
